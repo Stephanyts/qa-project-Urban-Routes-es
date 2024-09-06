6bd383177247c81daa5893468f8681282d73fd5a
Stephany Torres <stephanyts54@gmail.com>
2024-09-06 00:15:42 -0500

    Update UrbanRoutesPage.py

--- UrbanRoutesPage.py
+++ UrbanRoutesPage.py
@@ -8,7 +8,7 @@ class UrbanRoutesPage:
     # Localizador del botón pedir un taxi
     ask_taxi_button = (By.CLASS_NAME, 'button round')
     # Localizador del botón comfort
-    comfort_button = (By.CSS_SELECTOR, '.tcard.active')
+    comfort_button = (By.CSS_SELECTOR, '.tcard active')
     # localizador del titulo del elemento comfort
     comfort_title = (By.CSS_SELECTOR, '.tcard-title')
     # Localizador del campo numero de telefono
@@ -79,11 +79,6 @@ class UrbanRoutesPage:
     def comfort_rate_button(self):
         return self.driver.find_element(*self.comfort_button).click()
 
-
-    def is_comfort_button_selected(self):
-        return self.driver.find_element(*self.comfort_button).is_selected()
-
-
     def add_phone_number_method(self, phone_number):
         self.driver.find_element(*self.number_field_module).click()
         self.driver.find_element(*self.add_phone).send_keys(data.phone_number)
@@ -94,11 +89,6 @@ class UrbanRoutesPage:
     def confirm_phone_code(self):
         self.driver.find_element(*self.code_confirm_button).click()
 
-
-    def is_phone_code_confirmed(self):
-        return self.driver.find_element(*self.code_confirm_button).is_selected()
-
-
     def add_payment_method(self, card_number, card_code):
         self.driver.find_element(*self.payment_field).click()
         self.driver.find_element(*self.add_card_button).click()
@@ -107,27 +97,21 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.enable_add_button).click()
         self.driver.find_element(*self.added_card_button).click()
 
-    def is_enable_add_button_selected(self):
-        return self.driver.find_element(*self.enable_add_button).is_selected()
-
     def driver_message(self, driver_comment):
         self.driver.find_element(*self.driver_message_field).send_keys(data.message_for_driver)
 
-    def request_requirements(self):
+    def manta_requirements(self):
         self.driver.find_element(*self.Mantas_slider).click()
 
     def ice_cream_choise(self):
         self.driver.find_element(*self.ice_cream_counter).click()
         self.driver.find_element(*self.ice_cream_counter).click()
 
-
     def is_manta_selected(self):
         return self.driver.find_element(*self.Mantas_slider).is_selected()
-
 
     def end_request(self):
         self.driver.find_element(*self.send_taxi_request).click()
 
-
-    def is_send_request_button_enabled(self):
-        return self.driver.find_element(*self.send_taxi_request).is_selected()   
+    def is_send_request_button_is_displayed(self):
+        return self.driver.find_element(*self.send_taxi_request).is_displayed()
